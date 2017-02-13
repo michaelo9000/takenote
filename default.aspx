f633b678ab372c2a32c90f2577c0333da467314c
michaelo9000 <michael.ohara@hotmail.co.nz>
2017-02-14 08:51:51 +1300

    first

---
+++ default.aspx
@@ -0,0 +1,100 @@
+﻿<%@ Page validateRequest="false" Language="C#" AutoEventWireup="true" CodeBehind="default.aspx.cs" Inherits="WebApplication1.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Home</title>
+		<link rel="stylesheet" type="text/css" href="StyleSheet.css" />
+		<script type="text/javascript">
+		    function addLink() {
+		        var txta = document.getElementById("textBox");
+		        var pageName = document.getElementById("pageList").value + ".html";
+		        var start = "<a href='" + pageName + "'>";
+		        var end = "</a>";
+
+		        if (txta.selectionStart || txta.selectionStart == '0') {
+		            var startPos = txta.selectionStart;
+		            var endPos = txta.selectionEnd;
+		            var tag_seltxt = start + txta.value.substring(startPos, endPos) + end;
+		            txta.value = txta.value.substring(0, startPos) + tag_seltxt + txta.value.substring(endPos, txta.value.length);
+
+		            txta.setSelectionRange((endPos + start.length), (endPos + start.length));
+		            txta.focus();
+		        }
+		        return tag_seltxt;
+		    }
+		    function textHeader() {
+		        var txta = document.getElementById("textBox");
+		        var start = "<h3>";
+		        var end = "</h3>";
+
+		        if (txta.selectionStart || txta.selectionStart == '0') {
+		            var startPos = txta.selectionStart;
+		            var endPos = txta.selectionEnd;
+		            var tag_seltxt = start + txta.value.substring(startPos, endPos) + end;
+		            txta.value = txta.value.substring(0, startPos) + tag_seltxt + txta.value.substring(endPos, txta.value.length);
+
+		            txta.setSelectionRange((endPos + start.length), (endPos + start.length));
+		            txta.focus();
+		        }
+		        return tag_seltxt;
+		    }
+		    function lineBreak() {
+		        var txta = document.getElementById("textBox");
+		        var start = "</br>";
+
+		        if (txta.selectionStart || txta.selectionStart == '0') {
+		            var startPos = txta.selectionStart;
+		            var endPos = txta.selectionEnd;
+		            txta.value = txta.value.substring(0, startPos) + start + txta.value.substring(endPos, txta.value.length);
+
+		            txta.setSelectionRange((endPos + start.length), (endPos + start.length));
+		            txta.focus();
+		        }
+		        return start;
+		    }
+		    
+		  </script>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <asp:Label ID="errorLabel" runat="server"></asp:Label>
+        <div id="navbar">
+            <div id="navbarCenter">
+				<asp:DropDownList ID="redirectList" runat="server">
+                </asp:DropDownList>
+                <asp:Button ID="redirectButton" runat="server" Text="Open Page" OnClick="redirectButton_Click" />
+                <asp:Button ID="editButton" runat="server" Text="Edit Page" OnClick="editButton_Click" />
+                <asp:Button ID="deleteButton" runat="server" Text="Delete Page" OnClick="deleteButton_Click" Enabled="False" />
+                <asp:CheckBox ID="deleteEnable" runat="server" AutoPostBack="True" OnCheckedChanged="deleteEnable_CheckedChanged" />
+            </div>
+            <asp:Label ID="folderLabel" runat="server"></asp:Label>
+		</div>
+        <div id="sideBar">
+            <asp:DropDownList ID="pageList" runat="server">
+            </asp:DropDownList>	
+            <button id="linkButton" type="button" onClick="addLink();">Link Text</button>
+            <asp:FileUpload ID="imageUpload" runat="server" />
+            <asp:Button ID="imageSubmit" runat="server" Text="Upload" OnClick="imageSubmit_Click" />
+            <button id="headButton" type="button" onClick="textHeader();">Text is Header</button>
+            <button id="breakButton" type="button" onClick="lineBreak();">Line Break</button>
+            
+        </div>
+        <div id="sideBar2">
+            <asp:TextBox ID="newFolderBox" runat="server"></asp:TextBox>
+            <asp:Button ID="newFolderButton" runat="server" Text="Create Folder" OnClick="newFolderButton_Click" />
+            <asp:DropDownList ID="folderList" runat="server"></asp:DropDownList>
+            <asp:Button ID="folderButton" runat="server" Text="Open Folder" OnClick="folderButton_Click" />
+            <asp:Button ID="deleteFolderButton" runat="server" Text="Delete Active Folder" Enabled="False" OnClick="deleteFolderButton_Click" />
+        </div>
+        <div id="main">
+			<div id="container">
+                <asp:TextBox ID="titleBox" runat="server"></asp:TextBox>
+                <asp:Button ID="submitButton" runat="server" Text="Submit" OnClick="submitButton_Click" />&nbsp;
+				<asp:TextBox ID="textBox" runat="server" TextMode="MultiLine"></asp:TextBox>
+			</div>
+		</div>
+    </form>
+</body>
+</html>
---
+++ default.aspx.cs
@@ -0,0 +1,292 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+using System.Text;
+
+namespace WebApplication1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+       protected void Page_Load(object sender, EventArgs e)
+        {
+           errorLabel.Text = ""; 
+           if (folderList.SelectedValue == "")
+                loadDropDowns(this, e);
+        }
+
+       public void loadDropDowns(object sender, EventArgs e)
+       {
+           string foldersPath = Server.MapPath("folders.txt");
+           if (File.Exists(foldersPath))
+           {
+               folderList.Items.Clear();
+
+               string newItem;
+               string[] foldersList = File.ReadAllLines(foldersPath);
+               Array.Sort(foldersList);
+               for (int i = 0; i < foldersList.Length; i++)
+               {
+                   newItem = foldersList[i];
+                   folderList.Items.Add(newItem);
+               }
+           }
+           else
+               errorLabel.Text = "Folders file not found";
+        }
+
+        protected void submitButton_Click(object sender, EventArgs e)
+        {
+            bool overwriting = false;
+            string title = titleBox.Text;
+            string pageText = textBox.Text;
+
+            string newPath = Server.MapPath("Folders/" + folderLabel.Text + "/" + title + ".txt");
+            using (FileStream fs = File.Create(newPath))
+            {
+                Byte[] info = new UTF8Encoding(true).GetBytes(pageText);
+                fs.Write(info, 0, info.Length);
+            }
+            
+            string text = "";
+            string filePath = Server.MapPath("newPagePre.txt");
+            text = file(filePath);
+            string preText = text;
+            filePath = Server.MapPath("newPageMid.txt");
+            text = file(filePath);
+            string midText = text;
+            filePath = Server.MapPath("newPagePost.txt");
+            text = file(filePath);
+            string postText = text;
+
+            string newFile = preText + title + midText + title + ".png'/><br/>" + pageText + postText;
+
+            newPath = Server.MapPath("Folders/" + folderLabel.Text + "/"+title+".html");
+            using (FileStream fs = File.Create(newPath))
+            {
+                Byte[] info = new UTF8Encoding(true).GetBytes(newFile);
+                fs.Write(info, 0, info.Length);
+            }
+
+            string pagesFile = Server.MapPath("Folders/" + folderLabel.Text + "/pages.txt");
+            if (File.Exists(pagesFile))
+            {
+                string[] pagesArray = File.ReadAllLines(pagesFile);
+                List<string> pagesList = new List<string>(pagesArray);
+                foreach (string item in pagesList)
+                {
+                    if (item == title)
+                    overwriting = true;
+                }
+                if (overwriting != true)
+                {
+                    pagesList.Add(title);
+                    TextWriter writer = new StreamWriter(pagesFile);
+                    foreach (string item in pagesList)
+                    {
+                        writer.WriteLine(item);
+                    }
+                    writer.Close();
+                }
+                else
+                    errorLabel.Text = "overwritten"; 
+            }
+            string selectedFolder = folderLabel.Text;
+            loadPageDowns(this, e, selectedFolder);
+        }
+
+        public string file (string filePath)
+        {
+            StreamReader streamReader = new StreamReader(filePath);
+            string text = streamReader.ReadToEnd();
+            streamReader.Close();
+            return text;
+        }
+
+        protected void redirectButton_Click(object sender, EventArgs e)
+        {
+            string selected = redirectList.SelectedValue;
+            string page = "Folders/" + folderLabel.Text + "/" + selected + ".html";
+            string pagePath = Server.MapPath(page);
+            if (File.Exists(pagePath))
+                Response.Redirect(page);
+            else
+                errorLabel.Text = "Can't find page, WHOOPS";
+        }
+
+        protected void editButton_Click(object sender, EventArgs e)
+        {
+            string selected = redirectList.SelectedValue;
+            string filePath = Server.MapPath("Folders/" + folderLabel.Text + "/" + selected + ".txt");
+            string text = file(filePath);
+            textBox.Text = text;
+            titleBox.Text = selected;
+        }
+
+        protected void deleteButton_Click(object sender, EventArgs e)
+        {
+            string selected = redirectList.SelectedValue;
+            string htmlPath = Server.MapPath("Folders/" + folderLabel.Text + "/" + selected + ".html");
+            string txtPath = Server.MapPath("Folders/" + folderLabel.Text + "/" + selected + ".txt");
+            File.Delete(txtPath);
+            File.Delete(htmlPath);
+
+            string pagesFile = Server.MapPath("Folders/" + folderLabel.Text + "/pages.txt");
+            if (File.Exists(pagesFile))
+            {
+                string[] pagesArray = File.ReadAllLines(pagesFile);
+                List<string> pagesList = new List<string>(pagesArray);
+                TextWriter writer = new StreamWriter(pagesFile);
+                    foreach (string item in pagesList)
+                    {
+                        if (item != selected)
+                        writer.WriteLine(item);
+                    }
+                    writer.Close();
+                    string selectedFolder = folderLabel.Text;
+                    loadPageDowns(this, e, selectedFolder);
+            }
+        }
+
+        protected void newFolderButton_Click(object sender, EventArgs e)
+        {
+            string newPath;
+            bool overwriting = false;
+            string folderName = newFolderBox.Text;
+
+            if (newFolderBox.Text != "")
+            {
+                string pagesFile = Server.MapPath("folders.txt");
+                if (File.Exists(pagesFile))
+                {
+                    string[] pagesArray = File.ReadAllLines(pagesFile);
+                    List<string> pagesList = new List<string>(pagesArray);
+                    foreach (string item in pagesList)
+                    {
+                        if (item == folderName)
+                            overwriting = true;
+                    }
+                    if (overwriting != true)
+                    {
+                        pagesList.Add(folderName);
+                        TextWriter writer = new StreamWriter(pagesFile);
+                        foreach (string item in pagesList)
+                        {
+                            writer.WriteLine(item);
+                        }
+                        writer.Close();
+                        //Create new folder
+                        newPath = Server.MapPath("Folders/" + newFolderBox.Text);
+                        Directory.CreateDirectory(newPath);
+                        //Create pages.txt within new folder
+                        using (FileStream fs = File.Create(newPath + "/pages.txt"))
+                        { }
+                        //Create _StyleSheet file in new folder
+                        string filePath = Server.MapPath("_StyleSheet.css");
+                        string stylesheet = (file(filePath));
+                        newPath = Server.MapPath("Folders/" + newFolderBox.Text + "/_StyleSheet.css");
+                        using (FileStream filestream = File.Create(newPath))
+                        {
+                            Byte[] info = new UTF8Encoding(true).GetBytes(stylesheet);
+                            filestream.Write(info, 0, info.Length);
+                        }
+                        //reload folders DDList
+                        loadDropDowns(this, e);
+                    }
+                    else
+                        errorLabel.Text = "Folder already exists";
+                }
+            }
+            else
+                errorLabel.Text = "Give the folder a name dumgus";
+        }
+
+        protected void folderButton_Click(object sender, EventArgs e)
+        {
+           string selectedFolder = folderList.SelectedValue;
+           folderLabel.Text = selectedFolder;
+           loadDropDowns(this, e);
+           loadPageDowns(this, e, selectedFolder);
+        }
+        protected void loadPageDowns(object sender, EventArgs e, string selectedFolder)
+        {
+            string pagesPath = Server.MapPath("Folders/" + selectedFolder + "/pages.txt");
+            if (File.Exists(pagesPath))
+            {
+                pageList.Items.Clear();
+                redirectList.Items.Clear();
+
+                string newItem;
+                string[] pagesList = File.ReadAllLines(pagesPath);
+                Array.Sort(pagesList);
+                for (int i = 0; i < pagesList.Length; i++)
+                {
+                    newItem = pagesList[i];
+                    redirectList.Items.Add(newItem);
+                    pageList.Items.Add(newItem);
+                }
+            }
+            else
+                errorLabel.Text = "Pages file not found";
+        }
+
+        protected void deleteEnable_CheckedChanged(object sender, EventArgs e)
+        {
+            deleteButton.Enabled = !deleteButton.Enabled;
+            deleteFolderButton.Enabled = !deleteFolderButton.Enabled;
+        }
+
+        protected void deleteFolderButton_Click(object sender, EventArgs e)
+        {
+            if (folderLabel.Text == "")
+                errorLabel.Text = "choose a folder first";
+            else
+            {
+                string selectedFolder = folderLabel.Text;
+                string path = Server.MapPath("Folders/" + selectedFolder);
+                System.IO.DirectoryInfo di = new DirectoryInfo(path);
+
+                foreach (FileInfo file in di.GetFiles())
+                {
+                    file.Delete();
+                }
+                Directory.Delete(path);
+
+                string foldersFile = Server.MapPath("folders.txt");
+                if (File.Exists(foldersFile))
+                {
+                    string[] pagesArray = File.ReadAllLines(foldersFile);
+                    List<string> pagesList = new List<string>(pagesArray);
+                    TextWriter writer = new StreamWriter(foldersFile);
+                    foreach (string item in pagesList)
+                    {
+                        if (item != selectedFolder)
+                            writer.WriteLine(item);
+                    }
+                    writer.Close();
+                    folderLabel.Text = "";
+                    pageList.Items.Clear();
+                    redirectList.Items.Clear();
+                    loadDropDowns(this, e);
+                }
+            }
+        }
+
+        protected void imageSubmit_Click(object sender, EventArgs e)
+        {
+            string selectedPage = pageList.SelectedValue;
+            if (imageUpload.HasFile == false)
+                errorLabel.Text = "choose a picture";
+            else if (selectedPage == "")
+                errorLabel.Text = "open a folder";
+            else
+            {
+                string filePath = Server.MapPath("Folders/" + folderLabel.Text + "/" + selectedPage + ".png");
+                imageUpload.SaveAs(filePath);
+            }
+        }
+    }
+}
